9a7f8ba9092dad0de1415281d358e435f434a8d4
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-20 16:33:37 -0700

    user has gifts

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620221759) do
+ActiveRecord::Schema.define(version: 20150620233022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "gifts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.decimal  "price"
+    t.datetime "due_date"
+    t.string   "recipient"
+    t.integer  "user_id"
+    t.string   "hashed_id"
+    t.string   "gift_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
