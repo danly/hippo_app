9b34f565b232220d95948d3dde3d93abb3ed8fbe
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-20 14:28:20 -0700

    able to create users

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,25 @@
+class SessionController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
+  end
+end
