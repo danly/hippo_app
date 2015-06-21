1b8cb438ff264501eca8db436c56705c4162e888
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-20 17:18:01 -0700

    added redirect logic to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,22 @@ Rails.application.routes.draw do
   resources :users 
   resources :sessions, except: :new
 
+  # change root to landing page
+  # ladnding page should check current_user and redirect to dashboard
   root to: 'users#index'
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
   delete '/sessions', to: 'sessions#destroy', as: 'sign_out'
 end
 
 #       Prefix Verb   URI Pattern                  Controller#Action
+#        gifts GET    /gifts(.:format)             gifts#index
+#              POST   /gifts(.:format)             gifts#create
+#     new_gift GET    /gifts/new(.:format)         gifts#new
+#    edit_gift GET    /gifts/:id/edit(.:format)    gifts#edit
+#         gift GET    /gifts/:id(.:format)         gifts#show
+#              PATCH  /gifts/:id(.:format)         gifts#update
+#              PUT    /gifts/:id(.:format)         gifts#update
+#              DELETE /gifts/:id(.:format)         gifts#destroy
 #        users GET    /users(.:format)             users#index
 #              POST   /users(.:format)             users#create
 #     new_user GET    /users/new(.:format)         users#new
@@ -26,4 +36,4 @@ end
 #              DELETE /sessions/:id(.:format)      sessions#destroy
 #         root GET    /                            users#index
 #      sign_in GET    /sign_in(.:format)           sessions#new
-#              DELETE /sessions(.:format)          sessions#destroy
+#     sign_out DELETE /sessions(.:format)          sessions#destroy
